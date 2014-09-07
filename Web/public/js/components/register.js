6e779cca2e631bb3ef7babab5284d42b66bab8cd
Dennis Jin <desuvader@gmail.com>
2014-09-07 23:05:38 +0200

    Added basic calidation to register page

--- Web/public/js/components/register.js
+++ Web/public/js/components/register.js
@@ -1,28 +1,71 @@
 /** @jsx React.DOM */
 
 var registerForm = React.createClass({displayName: 'registerForm',
+	getInitialState: function(){
+		return {
+			usernameVal: null,
+			passwordVal: null,
+			emailVal: null,
+			emailValid: null
+		}
+	},
+	usernameChange: function(e){
+		this.setState({
+			usernameVal: e.target.value.replace(/\W+/g, '')
+		});
+	},
+	passwordChange: function(e){
+		this.setState({
+			passwordVal: e.target.value
+		});
+	},
+	emailChange: function(e){
+		e.persist();
+		this.setState({
+			emailVal: e.target.value
+		});
+		this.validateEmail(e);
+	},
+	validateEmail: _.debounce(function(e){
+		$.ajax({
+			type: 'get',
+			url: 'https://api.mailgun.net/v2/address/validate',
+			data: {
+				address: e.target.value,
+				api_key: 'pubkey-809b78e9259959060a4c93bae86b290a'
+			},
+			success: function(res){
+				this.setState({
+					emailValid: res.is_valid === true
+				});
+			}.bind(this),
+			error: function(err){
+				console.log(err);
+			}
+		});
+	}, 1000),
 	render: function(){
 		return (
 			React.DOM.form({id: "register-form", className: "logreg-form"}, 
 				React.DOM.div({className: "logreg-section-wrap"}, 
 					React.DOM.div({id: "logreg-form-hd"}, 
-						"Let's this get started."
+						"Let's get this started."
 					), 
 					React.DOM.div({id: "logreg-form-desc"}, 
-						"We don't need your e-mail. However, if you forget your credentials you will be shit out of luck."
+						"Most of Herro's features don't require that you have an email. However, if you forget your login credentials you will be shit out of luck."
 					)
 				), 
 				React.DOM.div({className: "logreg-section-wrap"}, 
-					React.DOM.div({className: "logreg-legend"}, "Username ", React.DOM.div({className: "logreg-legend-desc"}, "Required")), 
-					React.DOM.input({className: "logreg-input", type: "text", name: "username"})
+					React.DOM.div({className: "logreg-legend"}, "Username ", React.DOM.div({className: "logreg-legend-desc"}, "At least ", React.DOM.b(null, "3"), " chars. Only letters/numbers.")), 
+					React.DOM.input({className: "logreg-input", type: "text", name: "username", value: this.state.usernameVal, onChange: this.usernameChange})
 				), 
 				React.DOM.div({className: "logreg-section-wrap"}, 
-					React.DOM.div({className: "logreg-legend"}, "Password ", React.DOM.div({className: "logreg-legend-desc"}, "Required (at least 6 chars. long)")), 
-					React.DOM.input({className: "logreg-input", type: "password", name: "password"})
+					React.DOM.div({className: "logreg-legend"}, "Password ", React.DOM.div({className: "logreg-legend-desc"}, "At least ", React.DOM.b(null, "6"), " characters. Keep this secure.")), 
+					React.DOM.input({className: "logreg-input", type: "password", name: "password", value: this.state.passwordVal, onChange: this.passwordChange})
 				), 
 				React.DOM.div({className: "logreg-section-wrap"}, 
-					React.DOM.div({className: "logreg-legend"}, "E-mail ", React.DOM.div({className: "logreg-legend-desc"}, "Optional")), 
-					React.DOM.input({className: "logreg-input", type: "password", name: "password"})
+					React.DOM.div({className: "logreg-legend"}, "Email ", React.DOM.div({className: "logreg-legend-desc"}, React.DOM.b(null, "Optional"), ". You can add/remove it later.")), 
+					React.DOM.input({className: "logreg-input", type: "text", name: "email", value: this.state.emailVal, onChange: this.emailChange})
 				), 
 				React.DOM.div({className: "logreg-section-wrap"}, 
 					React.DOM.div({id: "logreg-submit"}, "Create My Account")
--- Web/src/js/components/register.jsx
+++ Web/src/js/components/register.jsx
@@ -1,28 +1,71 @@
 /** @jsx React.DOM */
 
 var registerForm = React.createClass({
+	getInitialState: function(){
+		return {
+			usernameVal: null,
+			passwordVal: null,
+			emailVal: null,
+			emailValid: null
+		}
+	},
+	usernameChange: function(e){
+		this.setState({
+			usernameVal: e.target.value.replace(/\W+/g, '')
+		});
+	},
+	passwordChange: function(e){
+		this.setState({
+			passwordVal: e.target.value
+		});
+	},
+	emailChange: function(e){
+		e.persist();
+		this.setState({
+			emailVal: e.target.value
+		});
+		this.validateEmail(e);
+	},
+	validateEmail: _.debounce(function(e){
+		$.ajax({
+			type: 'get',
+			url: 'https://api.mailgun.net/v2/address/validate',
+			data: {
+				address: e.target.value,
+				api_key: 'pubkey-809b78e9259959060a4c93bae86b290a'
+			},
+			success: function(res){
+				this.setState({
+					emailValid: res.is_valid === true
+				});
+			}.bind(this),
+			error: function(err){
+				console.log(err);
+			}
+		});
+	}, 1000),
 	render: function(){
 		return (
 			<form id="register-form" className="logreg-form">
 				<div className="logreg-section-wrap">
 					<div id="logreg-form-hd">
-						Let's this get started.
+						Let's get this started.
 					</div>
 					<div id="logreg-form-desc">
-						We don't need your e-mail. However, if you forget your credentials you will be shit out of luck.
+						Most of Herro's features don't require that you have an email. However, if you forget your login credentials you will be shit out of luck.
 					</div>
 				</div>
 				<div className="logreg-section-wrap">
-					<div className="logreg-legend">Username <div className="logreg-legend-desc">Required</div></div>
-					<input className="logreg-input" type="text" name="username" /> 
+					<div className="logreg-legend">Username <div className="logreg-legend-desc">At least <b>3</b> chars. Only letters/numbers.</div></div>
+					<input className="logreg-input" type="text" name="username" value={this.state.usernameVal} onChange={this.usernameChange} /> 
 				</div>
 				<div className="logreg-section-wrap">
-					<div className="logreg-legend">Password <div className="logreg-legend-desc">Required (at least 6 chars. long)</div></div>
-					<input className="logreg-input" type="password" name="password" /> 
+					<div className="logreg-legend">Password <div className="logreg-legend-desc">At least <b>6</b> characters. Keep this secure.</div></div>
+					<input className="logreg-input" type="password" name="password" value={this.state.passwordVal} onChange={this.passwordChange} /> 
 				</div>
 				<div className="logreg-section-wrap">
-					<div className="logreg-legend">E-mail <div className="logreg-legend-desc">Optional</div></div>
-					<input className="logreg-input" type="password" name="password" /> 
+					<div className="logreg-legend">Email <div className="logreg-legend-desc"><b>Optional</b>. You can add/remove it later.</div></div>
+					<input className="logreg-input" type="text" name="email" value={this.state.emailVal} onChange={this.emailChange} /> 
 				</div>
 				<div className="logreg-section-wrap">
 					<div id="logreg-submit">Create My Account</div>
